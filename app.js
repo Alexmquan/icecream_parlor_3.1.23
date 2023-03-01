7c7a1dbb2a33d84c0303f0ad5c47b8e461f6e5ba
Alex quan <alexmquan93@gmail.com>
2023-03-01 15:08:43 -0700

    Base done

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 const iceCream = [{
-  name: 'Cookie Dough',
-  image: 'https://celebratingsweets.com/wp-content/uploads/2014/04/Cookie-Dough-Ice-Cream-1-5.jpg',
-  price: 1
+  name: 'caramel',
+  quantity: 0,
+  price: 4
 }, {
-  name: 'Vanilla',
-  image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg',
-  price: 1
+  name: 'unicorn',
+  quantity: 0,
+  price: 4
 }, {
-  name: 'Strawberry',
-  image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
-  price: 2
-}],
-
-const toppings = [{
-  name: 'Sprinkles',
-  image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
-  price: 1
+  name: 'strawberry',
+  quantity: 0,
+  price: 3
+},
+{
+  name: 'vanilla',
+  quantity: 0,
+  price: 3
 }, {
-  name: 'Chocolate Chips',
-  image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
-  price: 2
-}],
-
-const containers = [{
-  name: 'Waffle Cone',
-  image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg',
-  price: 2
+  name: 'cookies',
+  quantity: 0,
+  price: 4
 }, {
-  name: 'Waffle Bowl',
-  image: 'http://images.wbmason.com/350/L_JOY66050.jpg',
+  name: 'ube',
+  quantity: 0,
   price: 4
 }]
 
+const toppings = [{
+  name: 'sprinkles',
+  quantity: 0,
+  price: 2
+}, {
+  name: 'gummy bears',
+  quantity: 0,
+  price: 3
+}, {
+  name: 'almonds',
+  quantity: 0,
+  price: 2
+}]
+
+const containers = [{
+  name: 'waffle cone',
+  quantity: 0,
+  price: 2
+}, {
+  name: 'cup',
+  quantity: 0,
+  price: 1
+}, {
+  name: 'sprinkle cone',
+  quantity: 0,
+  price: 3
+}]
+
+function drawIcecream() {
+
+}
+
+function vessel
