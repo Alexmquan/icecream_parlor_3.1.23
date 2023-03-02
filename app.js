99b74a7e02bc6b596ac9b591e4aa69e5b638fc41
Alex quan <alexmquan93@gmail.com>
2023-03-01 22:18:02 -0700

    fixed totaling issues

--- app.js
+++ app.js
@@ -76,20 +76,27 @@ function addIcecream(cream) {
   drawAll()
 }
 
+let allTotal = 0
 
 function drawAll() {
+  //NOTE - making your variable an empty string when you want it to be a number can cause problems when using math functions. MAKE SURE TO SET TO 0 INSTEAD!!!
   let thingName = ''
   let quantity = ''
   let cost = ''
   let total = ''
+  let topTotal = 0
+  let conTotal = 0
+  let iceTotal = 0
+  let newTotal = ''
 
-
+  //NOTE - making separate functions for each array set created an issue where they override each other. placing them all in the same draw function fixed this issue. This probably would not have been a problem if they were innerHTML ing to separate containers but because they are all going into the same <div>'s the draw function reset the contents of the divs everytime a different function was called to it.
   containers.forEach(con => {
     if (con.quantity) {
       quantity += `<h3>${con.quantity}</h3>`
       thingName += `<h3>${con.name}</h3>`
       cost += `<h3>$${con.price}</h3>`
       total += `<h3>$${con.quantity * con.price}</h3>`
+      conTotal += con.quantity * con.price
     }
 
   })
@@ -100,6 +107,7 @@ function drawAll() {
       thingName += `<h3>${top.name}</h3>`
       cost += `<h3>$${top.price}</h3>`
       total += `<h3>$${top.quantity * top.price}</h3>`
+      topTotal += top.quantity * top.price
     }
   })
 
@@ -109,49 +117,46 @@ function drawAll() {
       thingName += `<h3>${ice.name}</h3>`
       cost += `<h3>$${ice.price}</h3>`
       total += `<h3>$${ice.quantity * ice.price}</h3>`
+      iceTotal += ice.quantity * ice.price
     }
   })
 
+  allTotal = iceTotal + conTotal + topTotal
 
-
-  console.log(allTotal)
-
+  newTotal += `<h3>Cart Total: $${allTotal}</h3>`
+  document.getElementById('allTotal').innerHTML = newTotal
   document.getElementById('itemName').innerHTML = thingName
   document.getElementById('itemQuantity').innerHTML = quantity
   document.getElementById('itemCost').innerHTML = cost
   document.getElementById('itemTotal').innerHTML = total
 
-
+  // drawTotal()
 }
 
 
-function drawTotal() {
-  let allTotal = ''
-  let topTotal = ''
-  let conTotal = ''
-  let iceTotal = ''
-  containers.forEach(con => {
-    if (con.quantity)
-      conTotal += con.price
-  })
+//NOTE - failed attempt at trying to get a total. This caused weird math issues.
 
-}
+// function drawTotal() {
+//   let allTotal = 0
+//   let topTotal = 0
+//   let conTotal = 0
+//   let iceTotal = 0
 
+//   containers.forEach(con => {
+//     if (con.quantity)
+//       conTotal = con.price * con.quantity
+//   })
+//   toppings.forEach(top => {
+//     if (top.quantity)
+//       topTotal = top.price * top.quantity
+//   })
+//   iceCream.forEach(ice => {
+//     if (ice.quantity)
+//       iceTotal = ice.price * ice.quantity
+//   })
 
-// function drawTopping() {
-//   let thingName = ''
-//   let quantity = ''
-//   let cost = ''
-//   let total = ''
-
-
-
-//   document.getElementById('icecreamName').innerHTML = thingName
-//   document.getElementById('icecreamQuantity').innerHTML = quantity
-//   document.getElementById('icecreamCost').innerHTML = cost
-//   document.getElementById('icecreamTotal').innerHTML = total
-
-
+//   allTotal = topTotal + conTotal + iceTotal
+//   console.log(allTotal)
 // }
 
 
